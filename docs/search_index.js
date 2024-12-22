dd2588ba62125e070b7b115a1ce11ab23200d95d
Wande <wanderlustking11@gmail.com>
2024-12-22 15:54:36 -0500

    Instant Docs

---
+++ docs/search_index.js
@@ -0,0 +1,3 @@
+(function () {
+  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":[{"char":"f","kind":"Function","title":"Function"}],"name":"multiply","file":".","doc":"Multiplies two numbers together.","url":"././~/multiply.html","deprecated":false}]};
+})()
---
+++ lib.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Multiplies two numbers together.
+ */
+
+
+export function multiply(a:number, b: number) {
+    return a * b;
+}
+
+// pull up the terminal and run: `deno doc --html --name="My library" lib.ts
+
+// then look in the new 'docs' folder, and in the 'index.html'
+// either run locally, or 
