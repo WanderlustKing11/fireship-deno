f516e48a3f41fda726dee94f610450f3be44f25d
Wande <wanderlustking11@gmail.com>
2024-12-22 14:41:45 -0500

    testing

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
+// // Learning about meta.main // //
 
+// export function helloworld() {
+//   console.log('Main?', import.meta.main)
+//   return 'Hi Mom!'
+// }
 
-function helloworld() {
-  return 'Hi Mom!'
-}
+// if (import.meta.main) {
+//   console.log(helloworld());
+// }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log(helloworld());
-}
+//////////////////////////////////////////////
+
+// You can custom name any default imports
+
+import foo from './a.ts';
+import { bar, baz } from './b.ts';
+// or import {bar as customB } from './b.ts';
+
+foo
---
+++ other.ts
@@ -0,0 +1,3 @@
+import { helloworld } from "./main.ts";
+
+helloworld()
